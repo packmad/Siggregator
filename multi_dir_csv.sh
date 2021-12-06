31989d4430077d390b6b967c127d5c55c6977413
im-overlord04 <ninovitale04@gmail.com>
2021-12-06 10:57:56 +0100

    added: script for analyzing multiple directories

---
+++ multi_dir_csv.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+
+if [ "$#" -eq 2 ] && [[ -d $1 ]] && [[ -d $2 ]]; then
+	
+	IN_DIR=$(cd "$1" && pwd -P)
+	OUT_DIR=$(cd "$2" && pwd -P)
+		
+	for d in $IN_DIR/*/ ; do
+		OUT_FILE=$OUT_DIR/$(basename $d)
+		echo ./siggregator.sh $d $OUT_FILE.json
+		echo siggregator/results_to_csv.py $OUT_FILE.json $OUT_FILE.csv 
+		printf "\n"
+	done
+
+else
+	echo "Usage: multi_dir_csv.sh IN_DIR OUT_DIR"
+fi
